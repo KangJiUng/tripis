6c8d470aaf7298516c67173596627fc98abba010
KangJiUng <woong236406@gmail.com>
2025-12-16 21:44:17 +0900

    feat: header 및 navbar 컴포넌트 배치

--- app/(with-navbar)/layout.tsx
+++ app/(with-navbar)/layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
+import NavBar from "../components/navbar";
+import Header from "../components/header";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return <div><Header />{children}<NavBar /></div>;
 }
