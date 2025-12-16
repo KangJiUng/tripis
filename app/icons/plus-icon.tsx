11ece735dfb1bd9a8ea607dbcadcae1fdc6703af
KangJiUng <woong236406@gmail.com>
2025-12-16 21:43:28 +0900

    feat: navbar에 사용할 아이콘 파일 추가

---
+++ app/icons/plus-icon.tsx
@@ -0,0 +1,13 @@
+export default function PlusIcon() {
+  return (
+    <svg width="30" height="30" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg" xlinkHref="http://www.w3.org/1999/xlink">
+      <rect width="38.25" height="38.25" fill="url(#pattern0_133_5)"/>
+      <defs>
+      <pattern id="pattern0_133_5" patternContentUnits="objectBoundingBox" width="1" height="1">
+      <use xlinkHref="#image0_133_5" transform="scale(0.015625)"/>
+      </pattern>
+      <image id="image0_133_5" width="64" height="64" preserveAspectRatio="none" xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAQRJREFUeJzt2L1tg2AYReH7ei3IBJ7EZSZJF+yU2SELRPZGUN+U+ZEdkIw5SNynokD+ji6mQYqIiIitKuxkqw4fz60kdfuXs0rGWgi2j/52pHsWZ7v/MUBPdWCvgO1ff/mqQlp2xKFrkgHoAFoGoANoGYAOoGUAOoCWAegAWgagA2gZgA6gZQA6gJYBxm6wXbZPf77h3e3KOXPqbb9OGWD0O5ztJ0mfU35shdqquvx3Q16BCfecJb1JGh7cMqdBUjf29KV8Fs8rkAHoAFoGoANoGYAOoGUAOoCWAegAWgagA2gZgA6gZQA6gJYBwLOHG9eLIgd4v3G9HbYb2w3dEREREdv0BToe6gmzP2Q3AAAAAElFTkSuQmCC"/>
+      </defs>
+    </svg>
+  )
+}
