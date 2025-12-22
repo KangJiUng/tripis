2157a35409fd794a45ef2629b5e90918596e906e
KangJiUng <woong236406@gmail.com>
2025-12-22 15:30:56 +0900

    feat: 일정 등록 페이지 헤더 UI 구현

--- app/(with-header)/plan/create/page.tsx
+++ app/(with-header)/plan/create/page.tsx
@@ -1,3 +1,9 @@
+import PlanCreateHeader from '@/app/components/plan-create-header';
+
 export default function Page() {
-  return <div></div>
+  return (
+    <div>
+      <PlanCreateHeader /> 일정 등록
+    </div>
+  );
 }
