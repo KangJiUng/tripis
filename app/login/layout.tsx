5d95c6103bc4078613b486d71bb86f5230a59d4a
KangJiUng <woong236406@gmail.com>
2025-12-22 20:49:53 +0900

    feat: 로그인 페이지 UI 구현

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,9 +1,5 @@
 import { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <div className="container">
-      <main>{children}</main>
-    </div>
-  );
+  return <>{children}</>;
 }
