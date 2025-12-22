71156722fb62450688cd0e3732407d6b3b0f7ecb
KangJiUng <woong236406@gmail.com>
2025-12-22 14:57:43 +0900

    feat: 작성 페이지 헤더 UI 구현

--- app/(with-header)/expense/create/page.tsx
+++ app/(with-header)/expense/create/page.tsx
@@ -1,3 +1,10 @@
+import WriteHeader from '@/app/components/write-header';
+
 export default function Page() {
-  return <div></div>
+  return (
+    <div>
+      <WriteHeader title="가계부 작성" />
+      가계부 작성
+    </div>
+  );
 }
--- app/(with-header)/post/create/page.tsx
+++ app/(with-header)/post/create/page.tsx
@@ -1,3 +1,10 @@
+import WriteHeader from '@/app/components/write-header';
+
 export default function Page() {
-  return <div>게시글 작성</div>;
+  return (
+    <div>
+      <WriteHeader title="게시글 작성" />
+      게시글 작성
+    </div>
+  );
 }
