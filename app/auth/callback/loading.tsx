76f9c93e730bbe43da5eb8d49f609b6a66188f28
KangJiUng <woong236406@gmail.com>
2025-12-23 14:24:52 +0900

    feat: 비로그인 및 로그인 처리 로직 추가

--- app/auth/callback/loading.tsx
+++ app/auth/callback/loading.tsx
@@ -1,4 +1,4 @@
-export default function LoginLoading() {
+export default function Loading() {
   return (
     <div className="mx-auto flex min-h-screen w-full max-w-[600px] flex-col bg-[#5B5FED] shadow-[0px_7px_15px_0px_rgba(100,100,111,0.2)]">
       <div className="flex flex-1 flex-col items-center justify-center">
