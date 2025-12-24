e68e770ffd081b0cf77fbb8240dbce04fbe323d4
KangJiUng <woong236406@gmail.com>
2025-12-24 15:42:04 +0900

    test: 테스트용 로그아웃 버튼 추가

--- components/sidebar/user-menu.tsx
+++ components/sidebar/user-menu.tsx
@@ -1,3 +1,9 @@
+import LogoutButtonTest from '../logout-button-test';
+
 export default function UserMenu() {
-  return <div>로그인 ui</div>;
+  return (
+    <div>
+      로그인 ui <LogoutButtonTest />
+    </div>
+  );
 }
