da77bad2995bcfcad8eba6672c2a977e1744b8bf
KangJiUng <107825793+KangJiUng@users.noreply.github.com>
2025-12-26 18:23:59 +0900

    feat: 커뮤니티 게시글 탭 UI 구현 (#20)
    * feat: 공통 피드 UI 구현
    
    * feat: 커뮤니티 게시글 탭 UI 구현
    
    * feat: 텍스트 스타일 추가 및 패딩 수정

---
+++ components/feed/common-feed/common-card.tsx
@@ -0,0 +1,21 @@
+export default function CommonCard() {
+  return (
+    <div className="flex h-30 w-full">
+      <div className="flex w-full">
+        <div className="flex-1 pr-3">
+          <div className="text-bold15 pb-1">출국 2시간 전 공항</div>
+          <div className="text-regular13 h-10 overflow-hidden">
+            토요일 아침에 들어가면 실내주차 자리 잡을 수 있을까요? 아니면 바로 만차일까요?토요일 아침에 들어가면
+            실내주차 자리 잡을 수 있을까요? 아니면 바로 만차일까요?토요일 아침에 들어가면 실내주차 자리 잡을 수
+            있을까요? 아니면 바로 만차일까요?토요일 아침에 들어가면 실내주차 자리 잡을 수 있을까요? 아니면 바로
+            만차일까요?
+          </div>
+          <div className="text-regular12 pt-6 text-[#c4c4c4]">홍길동 • 2025.8.22</div>
+        </div>
+        <div className="pt-1">
+          <div className="h-[100px] w-[100px] items-center justify-center rounded-[5px] bg-gray-300" />
+        </div>
+      </div>
+    </div>
+  );
+}
