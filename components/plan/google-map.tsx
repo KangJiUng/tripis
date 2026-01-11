349c693666b9130f231ddd53572c18009c514a6e
KangJiUng <woong236406@gmail.com>
2026-01-11 11:54:08 +0900

    fix: 구글맵 중복 호출 문제 해결

--- components/plan/google-map.tsx
+++ components/plan/google-map.tsx
@@ -1,20 +1,26 @@
 'use client';
 
-import { GoogleMap as Map, LoadScript } from '@react-google-maps/api';
+import { GoogleMap as Map, useJsApiLoader } from '@react-google-maps/api';
 import type { LatLng } from '@/types';
 
 export default function GoogleMap({ className, center }: { className: string; center: LatLng }) {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+  });
+
+  if (!isLoaded) return null; // 로더가 완료되었을 때만 렌더링
+
   return (
-    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-      <Map
-        mapContainerClassName={className}
-        center={center}
-        zoom={8}
-        options={{
-          disableDefaultUI: true,
-          clickableIcons: false,
-        }}
-      />
-    </LoadScript>
+    <Map
+      key={`${center.lat}-${center.lng}`}
+      mapContainerClassName={className}
+      center={center}
+      zoom={8}
+      options={{
+        disableDefaultUI: true,
+        clickableIcons: false,
+      }}
+    />
   );
 }
