90657bdd9cb43d9368bae35a3b6a9e9dcf095325
KangJiUng <woong236406@gmail.com>
2026-01-16 14:01:02 +0900

    feat: 커뮤니티 포스트 GET 구현

--- components/feed/common-feed/common-card.tsx
+++ components/feed/common-feed/common-card.tsx
@@ -1,21 +1,33 @@
-export default function CommonCard() {
+type CommonPost = {
+  post_id: string;
+  title: string;
+  content: string;
+  created_at: string;
+  user_id: string;
+  image_urls: string[];
+};
+
+export default function CommonCard({ post }: { post: CommonPost }) {
+  const hasImage = post.image_urls && post.image_urls.length > 0;
+  const thumbnail = hasImage ? post.image_urls[0] : null;
+
   return (
-    <div className="flex h-30 w-full">
-      <div className="flex w-full">
-        <div className="flex-1 pr-3">
-          <div className="text-bold15 pb-1">출국 2시간 전 공항</div>
-          <div className="text-regular13 h-10 overflow-hidden">
-            토요일 아침에 들어가면 실내주차 자리 잡을 수 있을까요? 아니면 바로 만차일까요?토요일 아침에 들어가면
-            실내주차 자리 잡을 수 있을까요? 아니면 바로 만차일까요?토요일 아침에 들어가면 실내주차 자리 잡을 수
-            있을까요? 아니면 바로 만차일까요?토요일 아침에 들어가면 실내주차 자리 잡을 수 있을까요? 아니면 바로
-            만차일까요?
-          </div>
-          <div className="text-regular12 pt-6 text-[#c4c4c4]">홍길동 • 2025.8.22</div>
-        </div>
-        <div className="pt-1">
-          <div className="h-[100px] w-[100px] items-center justify-center rounded-[5px] bg-gray-300" />
+    <div className="mt-3 flex w-full">
+      <div className="flex-1 pr-3">
+        <div className="text-bold15 pt-1">{post.title}</div>
+
+        <div className="text-regular13 h-10 overflow-hidden text-ellipsis">{post.content}</div>
+
+        <div className="text-regular12 pt-6 pb-3 text-[#c4c4c4]">
+          홍길동 • {new Date(post.created_at).toLocaleDateString()}
         </div>
       </div>
+
+      {thumbnail && (
+        <div className="shrink-0 pt-1.5">
+          <img src={thumbnail} alt="post thumbnail" className="h-[100px] w-[120px] rounded-md object-cover" />
+        </div>
+      )}
     </div>
   );
 }
